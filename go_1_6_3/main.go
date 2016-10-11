c57a93d143abac9259c2128a5821952d0495e166
name <freiny50@gmail.com>
2016-10-11 03:32:35 -0500

    Update go repl's

--- go_1_6_3/main.go
+++ go_1_6_3/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	fmt.Println("hello")
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
